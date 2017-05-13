098967bce4548b133706db2ad3dc8dbf5f9bc107
smnkrt <szymon.krywult@netguru.pl>
2017-05-13 14:42:22 +0200

    add snake_case_method_names option support

--- lib/hash_mangler.rb
+++ lib/hash_mangler.rb
@@ -2,21 +2,27 @@ require 'hash_mangler/version'
 
 module HashMangler
   class SimpleStruct
-    def initialize
+    def initialize(snake_case_method_names = true)
+      @snake_case_method_names = snake_case_method_names
       @hash = {}
     end
 
     def []=(key, val)
-      key = key_to_snake_case(key)
+      key = format_key(key)
       @hash[key] = val
       define_singleton_method(key) { @hash[key] }
     end
 
     def [](key)
-      @hash[key_to_snake_case(key)]
+      @hash[format_key(key)]
     end
 
     private
+
+    def format_key(key)
+      return key unless @snake_case_method_names
+      key_to_snake_case(key)
+    end
 
     def key_to_snake_case(key)
       key
@@ -32,13 +38,14 @@ module HashMangler
 
   class Mangler
     def initialize(options = {})
+      @snake_case_method_names = options.fetch(:snake_case_method_names, true)
       @value_mangler = options.fetch(:value_mangler, proc { |o| o })
     end
 
     def mangle(a)
       return @value_mangler.(a) unless a.is_a?(Hash) || a.is_a?(Array)
       return a.map { |v| mangle(v) } if a.is_a?(Array)
-      r = HashMangler::SimpleStruct.new
+      r = HashMangler::SimpleStruct.new(@snake_case_method_names)
       a.each { |k, v| r[k] = mangle(v) }
       r
     end
--- spec/hash_mangler_spec.rb
+++ spec/hash_mangler_spec.rb
@@ -5,7 +5,11 @@ RSpec.describe HashMangler::Mangler do
     let(:test_hash) do
       {
         a: { b: 1 },
-        c: [{ d: { e: '2' } }, { f: 'g' }, 'h'],
+        c: [
+          { d: { e: '2' } },
+          { f: 'g' },
+          'h'
+        ],
         camelCaseName: 'j'
       }
     end
@@ -43,6 +47,24 @@ RSpec.describe HashMangler::Mangler do
 
       it 'raises NoMethodError when wrong method name is provided' do
         expect { subject.a.c }.to raise_error(NoMethodError)
+      end
+    end
+
+    context 'snake_case_method_names option provided' do
+      let(:options) { { snake_case_method_names: false } }
+      subject { HashMangler::Mangler.new(options).mangle(test_hash) }
+
+      it 'mangle works', :aggregate_failures do
+        expect(subject.a.b).to eq(test_hash[:a][:b])
+        expect(subject.c).to be_a(Array)
+        expect(subject.c[0].d.e).to eq(test_hash[:c][0][:d][:e])
+        expect(subject.c[1].f).to eq(test_hash[:c][1][:f])
+        expect(subject.c[2]).to eq(test_hash[:c][2])
+        expect(subject.camelCaseName).to eq(test_hash[:camelCaseName])
+      end
+
+      it 'raises NoMethodError when wrong method name is provided' do
+        expect { subject.camel_case_name }.to raise_error(NoMethodError)
       end
     end
   end
